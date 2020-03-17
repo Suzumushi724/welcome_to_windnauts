7fb749360784f508058c3605d757233f14761f74
沖野峻也 <okinoshunnari@okinoshunnarinoMacBook-Air.local>
2020-03-18 03:37:23 +0900

    オサレにしてみた2

--- fadein.js
+++ fadein.js
@@ -2,7 +2,8 @@ function showElementAnimation() {
 
     var element = document.getElementsByClassName('contents');
     if(!element) return; // 要素がなかったら処理をキャンセル
-    
+    var images = new Array('url(images/wind.jpg)','url(images/avionics.jpg)','url(images/ski.jpg)','url(images/aside.jpg)');
+    var colors = new Array('#36ab77','#e04747', '#e7ff03', '#035fff');
     var showTiming;
     if(window.innerHeight > 768){
         showTiming = 200;
@@ -16,10 +17,24 @@ function showElementAnimation() {
         var elemY = element[i].getBoundingClientRect().top; 
         if(WindowHeight - showTiming > elemY) {
             element[i].classList.add('show');
+            if(i==0){
+                element[i].style.backgroundImage = 'none';
+                //document.body.style.backgroundImage = 'none';
+            }
+            else{
+                element[i].style.backgroundImage= images[i];
+                //document.body.style.backgroundImage = images[i];
+                
+            }
         } 
         
         else if(WindowHeight < elemY) {
             element[i].classList.remove('show');
+        }
+
+        else{
+            document.body.style.backgroundColor = '#000';
+            document.body.style.backgroundImage = 'none';
         }
         
     }
